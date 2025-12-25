3aa70d1a6a2cbf593de9a531af25152c27f50cb3
Mist <114344332+MIST145@users.noreply.github.com>
2025-12-25 04:13:55 +0000

    Update gifs.lua - Added 1 new entries

--- database/gifs.lua
+++ database/gifs.lua
@@ -1,3 +1,7 @@
 -- FiveManage Gifs
 return {
+    ["not_exist"] = {
+        label = "not_exist",
+        image = "https://raw.githubusercontent.com/MIST145/Props_pictures/main/ec-dealership/not_exist.gif"
+    }
 }
