5f1bcf0da1185c0ba645b5140ca55fea325976e5
Mist <114344332+MIST145@users.noreply.github.com>
2026-01-03 16:00:38 +0000

    Delete: database/gifs.lua

--- database/gifs.lua
+++
@@ -1,7 +0,0 @@
--- FiveManage Gifs
-return {
-    ["not_exist"] = {
-        label = "not_exist",
-        image = "https://raw.githubusercontent.com/MIST145/Props_pictures/main/ec-dealership/not_exist.gif"
-    }
-}
